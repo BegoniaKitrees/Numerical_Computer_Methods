29f4a5aaac354518b0ce01602bcbbae297cd011c
海棠千树 <1149707218@qq.com>
2017-05-15 23:58:48 +0800

    迭代法-牛顿迭代法-单点弦截法-双点弦截法
    完成主要代码

--- 迭代法-牛顿迭代法-单点弦截法-双点弦截法/迭代法-牛顿迭代法-单点弦截法-双点弦截法/Source.cpp
+++ 迭代法-牛顿迭代法-单点弦截法-双点弦截法/迭代法-牛顿迭代法-单点弦截法-双点弦截法/Source.cpp
@@ -9,7 +9,8 @@
 using namespace std;
 void iteration()
 {
-	//迭代公式 x2 = e^(-x1)
+	//简单迭代法
+	//迭代公式: x2 = e^(-x1)
 	double x1 = 0.5;
 	double x2 = 0.0;
 	double temp = 0.0;
@@ -28,7 +29,9 @@ void iteration()
 }
 void Newton_iteration()
 {
-	//迭代公式：由 x=e^(-x)----> x*e^x-1=0 -----> f(x) = x*e^x-1 ---->f'(x) = e^x + x*e^x
+	//牛顿迭代法
+	//迭代公式: x2 = x1 -f(x1)/f'(x1)
+	//由 x=e^(-x)----> x*e^x-1=0 -----> f(x) = x*e^x-1 ---->f'(x) = e^x + x*e^x
 	//x2 = x1 - (x1 - exp(-x1)) / (1 + x1)
 	double x1 = 0.5;
 	double x2 = 0.0;
@@ -46,10 +49,52 @@ void Newton_iteration()
 	}
 	cout << "Final Answer: " << setprecision(5) << x2 << endl;
 }
+void single_secant()
+{
+	//单点弦截法:将牛顿迭代法中的f'(x)替换为固定点x0和动点x1之间的割线斜率
+	//迭代公式: x2 = x1 - f(x1)*(x1 - x0) / (f(x1) - f(x0))
+	double x1 = 0.6;
+	double x2 = 0.0;
+	double temp = 0.0;
+	int count = 0;
+	cout << "Secant:" << endl;
+	while (abs(x1 - temp) > 0.5e-5)
+	{
+		x2 = x1 - (x1 - exp(-x1))*(x1 - 0.5) / (x1 - 0.5 - exp(-x1) + exp(-0.5));
+		count++;
+		temp = x1;
+		x1 = x2;
+		cout << "Step :" << count << " " << x2 << endl;
+		cout << "D-value :" << abs(x1 - temp) << endl;
+	}
+	cout << "Final Answer: " << setprecision(5) << x2 << endl;
+}
+void double_secant()
+{
+	//双点弦截法: 将单点弦截法中的固定点x0换为前一次计算出的x1
+	//迭代公式: x2 = x1 - f(x1)*(x1 - x0) / (f(x1) - f(x0))
+	double x1 = 0.5;
+	double x2 = 0.0;
+	double temp = 0.0;
+	int count = 0;
+	cout << "Secant:" << endl;
+	while (abs(x1 - temp) > 0.5e-5)
+	{
+		x2 = x1 - (x1 - exp(-x1))*(x1 - temp) / (x1 - temp - exp(-x1) + exp(-temp));
+		count++;
+		temp = x1;
+		x1 = x2;
+		cout << "Step :" << count << " " << x2 << endl;
+		cout << "D-value :" << abs(x1 - temp) << endl;
+	}
+	cout << "Final Answer: " << setprecision(5) << x2 << endl;
+}
 int main()
 {
 	iteration();
 	Newton_iteration();
+	single_secant();
+	double_secant();
 	system("pause");
 	return 0;
 }
