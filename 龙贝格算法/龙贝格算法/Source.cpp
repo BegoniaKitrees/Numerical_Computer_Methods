0f6d676e456fc317a696e5a3a9787484fe791e62
海棠千树 <1149707218@qq.com>
2017-06-02 17:29:57 +0800

    龙贝格算法

--- 龙贝格算法/龙贝格算法/Source.cpp
+++ 龙贝格算法/龙贝格算法/Source.cpp
@@ -1,58 +1,22 @@
 #include<iostream>
 #include<math.h>
-float f(float x)
-{	
-	float y;
-	y = exp(-x*x);
-	return(y);
+using namespace std;
+double fun(double x)
+{
+	return pow(x, 1.5);
 }
 int main()
 {
-	float a, b, h, x, R, eps, S, T[20];
-	int i, j, m = 2, g;
-	printf("\ninput a,b,eps:\n");
-	scanf("%f,%f,%f", &a, &b, &eps);
-	printf("a=%f,b=%f,eps=%f\n", a, b, eps);
-	h = b - a;
-	T[1] = h*(f(a) + f(b)) / 2;
-	T[2] = (T[1] + h*f(a + h / 2)) / 2;
-	T[1] = (4 * T[2] - T[1]) / 3;
-	for (i = 2; i <= 3; i++)
-	{
-		S = 0;
-		for (j = 1; j <= 2 * m - 1; j = j + 2)
-		{
-			x = a + j*h / m / 2;
-			S = S + f(x);
-		}
-		T[i + 1] = (T[i] + h*S / m) / 2;
-		g = 1;
-		for (j = i; j >= 1; j--)
-		{
-			g = 4 * g;
-			T[j] = (T[j + 1] * g - T[j]) / (g - 1);
-		}
-		m = 2 * m;
-	}
-	R = T[1];
-	for (i = 4; ; i++)
-	{
-		S = 0;
-		for (j = 1; j <= 2 * m - 1; j = j + 2)
-		{
-			x = a + j*h / m / 2;
-			S = S + f(x);
-		}
-		T[i + 1] = (T[i] + h*S / m) / 2;
-		g = 1;
-		for (j = i; j >= i - 2; j--)
-		{
-			g = 4 * g;
-			T[j] = (T[j + 1] * g - T[j]) / (g - 1);
-		}
-		if (fabs(T[i - 2] - R)<eps)break;
-		m = 2 * m;
-		R = T[i - 2];
-	}
-	printf("I=%f\n", T[i - 2]);
+	double T[6];
+	double S[6];
+	double C[6];
+	double R[6];
+	double a = 1.0;
+	double b = 2.0;
+	double h = b - a;
+	cout << "h: " << h << endl;
+	T[1] = h / 2 * (fun(a) + fun(b));
+	double sum = 0;
+	system("pause");
+	return 0;
 }
